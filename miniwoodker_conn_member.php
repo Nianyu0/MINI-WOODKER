c12887ab8917bde1a39ab9aa7dd0e042cb147781
Nianyu0 <a0909230216@gmail.com>
2025-04-10 14:35:53 +0800

    new projects

--- miniwoodker_conn_member.php
+++ miniwoodker_conn_member.php
@@ -1,11 +1,10 @@
 <?php
  
-    $server      =  "localhost";
-    $username    =  "admin";
-    $password    =  "789456";
-    $dbname      =  "miniwoodker";
-
-    $conn  =  mysqli_connect(  $server  ,  $username  ,  $password  ,  $dbname  )  ;
+    $server      =  "sql102.infinityfree.com";
+    $username    = "if0_38646795";
+    $password    = "Nul00221166"; 
+    $dbname      = "if0_38646795_db";
+    $conn        =  mysqli_connect(  $server  ,  $username  ,  $password  ,  $dbname  )  ;
 
     if (  !$conn  ){      
                         echo   json_encode(   [  "statue" => false  ,  "message" => "連線失敗"  ]     ) ;    
